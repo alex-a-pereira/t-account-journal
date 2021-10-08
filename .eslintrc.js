4960ba0f0fce45b0eeb4cfeadf3a27dbd424238d
Alex Pereira <alexapereira14@gmail.com>
2021-10-08 14:46:55 -0400

    add linter

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'jest',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/jsx-indent': ['error', 2],
+    'react/prop-types': [
+      2, { ignore: ['children'] }],
+    // api responses are in camel case, if this was real code
+    // then just turning it off I would have found a better solution
+    camelcase: 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error']
+  },
+  ignorePatterns: [
+    'node_modules/**'
+  ],
+  overrides: [
+    {
+      files: [
+        '**/*.test.ts',
+        '**/*.test.tsx'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
+}
