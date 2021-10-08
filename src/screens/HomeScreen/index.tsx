a6ea47b0914633d8760fdb7f70ad3dfebff01f3e
alex pereira <alexapereira14@gmail.com>
2021-10-08 18:17:46 -0400

    create container for general ledger

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -7,6 +7,12 @@ import './HomeScreen.scss'
 export const HomeScreen: React.FC = () => {
   return (
     <div className="screen-container">
+      <h2>General Ledger</h2>
+      <div className='general-ledger-section'>
+        <p>general ledger here :)</p>
+      </div>
+      <hr />
+      <h2>T-Accounts</h2>
       <div className='t-accounts-section'>
         <TAccount />
         <TAccount />
