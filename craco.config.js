d3d1ef9f8d77656e88ec98efb96104f280371fc6
alex pereira <alexapereira14@gmail.com>
2021-10-08 16:42:49 -0400

    global scss files

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,28 @@
 const CracoAlias = require('craco-alias')
 
 module.exports = {
+  style: {
+    sass: {
+      loaderOptions: {
+        additionalData: (content, loaderContext) => {
+          const additionalImports = [
+            'src/setup/styles/variables.scss',
+            'src/setup/styles/globals.scss'
+          ]
+
+          const importsForThisFile = additionalImports
+            .filter(imp => !loaderContext.resourcePath.endsWith(imp))
+            .map(imp => `@import "${imp}";`)
+            .join('\n')
+
+          return `
+            ${importsForThisFile}
+            ${content}
+          `
+        }
+      }
+    }
+  },
   plugins: [
     {
       plugin: CracoAlias,
