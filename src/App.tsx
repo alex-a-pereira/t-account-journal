c8e6a941c48c2e5da9d2e0fb9d1ab60e16c95410
alex pereira <alexapereira14@gmail.com>
2021-10-08 16:43:16 -0400

    feat: rename App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import './App.css'
+import './App.scss'
 // nav
 import { AppRouter } from '@setup/Router/router'
 
 function App () {
   return (
-    <AppRouter>
-      {/* placeholder for future stuff */}
-      <div />
-    </AppRouter>
+    <div className='App'>
+      <AppRouter>
+        {/* placeholder for future stuff */}
+        <div />
+      </AppRouter>
+    </div>
   )
 }
 
