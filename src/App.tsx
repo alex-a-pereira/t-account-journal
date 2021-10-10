60d523ad91c41ce8623106587142b40426212365
alex pereira <alexapereira14@gmail.com>
2021-10-10 12:44:09 -0400

    feat: site header

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import './App.scss'
 // context providers
 import { JournalDataProvider } from '@providers/JournalDataProvider'
+// UI
+import { SiteHeader } from '@components/SiteHeader/SiteHeader'
 // nav
 import { AppRouter } from '@setup/Router/router'
 
 function App () {
   return (
     <div className='App'>
+      <SiteHeader />
       <JournalDataProvider>
         <AppRouter>
           {/* placeholder for future stuff */}
---
+++ src/components/SiteHeader/SiteHeader.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import './SiteHeader.scss'
+
+export const SiteHeader: React.FC = () => {
+  return (
+    <div className='header-container'>
+      <div className='site-name-container'>
+        <h2>SITE NAME</h2>
+      </div>
+
+      <div className='main-items-container'>
+        <p>about us</p>
+        <p>how to use</p>
+        <p>something</p>
+      </div>
+
+      <div className='stuff-on-right'>
+        <p>anything else</p>
+      </div>
+    </div>
+  )
+}
